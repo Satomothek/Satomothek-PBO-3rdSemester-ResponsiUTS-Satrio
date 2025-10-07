1d347cde4e6cfd9ae0145ec387b151bfef78771b
Satomo <satrioutomo344@gmail.com>
2025-10-07 19:32:30 +0700

    Add files via upload

---
+++ Responsi_PBOUTS/src/main/java/Responsi_PBO_UTS_2440506063/PegawaiKontrak.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Responsi_PBO_UTS_2440506063;
+
+/**
+ *
+ * @author Satomo
+ */
+public class PegawaiKontrak extends Pegawai {
+    private String lamaKontrak; // contoh: "12 bulan"
+
+    public PegawaiKontrak(String namaPegawai, double gaji, String lamaKontrak) {
+        super(namaPegawai, gaji);
+        this.lamaKontrak = lamaKontrak;
+    }
+
+    public String getLamaKontrak() {
+        return lamaKontrak;
+    }
+
+    public void setLamaKontrak(String lamaKontrak) {
+        this.lamaKontrak = lamaKontrak;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Lama Kontrak: " + lamaKontrak);
+    }
+}
