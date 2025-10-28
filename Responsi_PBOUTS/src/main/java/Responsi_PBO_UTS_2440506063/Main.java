40b4c169dc62c324c536f373feb74cc21a16a8b6
Satomo <satrioutomo344@gmail.com>
2025-10-28 12:49:53 +0700

    Update Main.java

--- Responsi_PBOUTS/src/main/java/Responsi_PBO_UTS_2440506063/Main.java
+++ Responsi_PBOUTS/src/main/java/Responsi_PBO_UTS_2440506063/Main.java
@@ -10,19 +10,16 @@ package Responsi_PBO_UTS_2440506063;
  */
 public class Main {
     public static void main(String[] args) {
-        // Output Produk
         System.out.println("1. Output Produk:");
         Elektronik produk1 = new Elektronik("Laptop", 15000000, 2);
         produk1.tampilkanInfo();
         System.out.println();
 
-        // Output Pegawai
         System.out.println("2. Output Pegawai:");
         PegawaiTetap pegawai1 = new PegawaiTetap("Satrio", 5000000, 1000000);
         pegawai1.tampilkanInfo();
         System.out.println();
 
-        // Output Polimorfisme
         System.out.println("3. Output Polimorfisme:");
         Produk produk2 = new Makanan("Snack", 15000, "2023-12-30");
         produk2.tampilkanInfo();
@@ -32,3 +29,4 @@ public class Main {
         pegawai2.tampilkanInfo();
     }
 }
+
